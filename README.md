7ee39109efd7da370cb93f10d9e57306b1ac8761
phamdanghoa <phamdanghoa040500@gmail.com>
2021-11-26 16:03:19 +0700

    BT CN

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sorfware Testing - Katalon
+# Create 1 TestSuites includes 5 TestCases for Login page online HCMUE
+# Phạm Đăng Hoa - 44.01.104.090
---
+++ Test Suites/Login Test.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>20188a81-9dff-499e-998d-252805132ff9</testSuiteGuid>
+   <testCaseLink>
+      <guid>13f77546-70cd-4a44-86b3-0dbfc337cbc3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Login_allEmpty</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f6494fe9-c236-4ec5-8325-87a017152204</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_invalidAll</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c196623-4e97-440c-9ec3-6240a8aea07c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_invalidPassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bc9f989b-51ca-48d2-bb59-c938776735fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_invalidUserName</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>91f6bbe8-58d8-46b9-bab2-808b8cec6ade</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_Success</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
